956f566b09fe936bbac6f64d07ce1aacd843f5fb
Preet Panchal <preet2020@icloud.com>
2024-07-19 12:24:57 -0400

    Initial Commit

---
+++ KindKitchen/KindKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindKitchenApp.swift
+//  KindKitchen
+//
+//  Created by Preet Panchal on 2024-07-19.
+//
+
+import SwiftUI
+
+@main
+struct KindKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
